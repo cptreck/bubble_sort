9bcbb221410b8e51aad68b5d366045dfaf56dda1
Jesse Caccavale <58094016+cptreck@users.noreply.github.com>
2024-02-01 17:02:36 -0500

    playing around with #each_index

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -3,7 +3,18 @@
 # (using #sort would be pretty pointless, wouldn’t it?).
 
 def bubble_sort (arr)
-
+    low = 0
+    high = 0
+    sorted = arr
+    sorted.each_index do |i|
+        unless sorted[i + 1] == nil
+            if sorted[i] > sorted[i + 1]
+                high = sorted[i]
+                sorted[i] = sorted[i + 1]
+                sorted[i + 1] = high
+            end
+        end
+    end     
 end
 
 p bubble_sort([4,3,78,2,0,2])
