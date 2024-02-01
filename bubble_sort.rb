269a44cd39924941afbf2584fd478e60e104234c
Jesse Caccavale <58094016+cptreck@users.noreply.github.com>
2024-02-01 16:26:35 -0500

    initial commit

---
+++ bubble_sort.rb
@@ -0,0 +1,9 @@
+# Build a method #bubble_sort that takes an array and returns 
+# a sorted array. It must use the bubble sort methodology 
+# (using #sort would be pretty pointless, wouldn’t it?).
+
+def bubble_sort (arr)
+
+end
+
+p bubble_sort([4,3,78,2,0,2])
